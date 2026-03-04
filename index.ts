86508c47044208e4af4c6d94462568253efad15a
JulsKawa <24568624+JulsKawa@users.noreply.github.com>
2026-03-04 23:41:14 +0100

    Remove an error from the cherry pick.

--- index.ts
+++ index.ts
@@ -3,5 +3,3 @@ export { Funds } from "./src/types/Funds";
 export { TokenPrecision } from "./src/types/TokenPrecision";
 export { Address } from "./src/types/Address";
 export { MAX_U64 } from "./src/utils/int";
-export * from "./src/api/client";
-export * from "./src/api/types";
