dc60e45430c85d324ebcf77fb93c5daf19cbce15
CoinFu Master Shifu <145641460+CoinFuMasterShifu@users.noreply.github.com>
2026-03-05 19:14:52 +0100

    member generators for TransactionBytes instead of static factories

--- src/types/TransactionBytes.ts
+++ src/types/TransactionBytes.ts
@@ -14,6 +14,143 @@ export class TransactionContext {
         public readonly feeE8: bigint,
         public readonly nonceId: number
     ) {}
+
+    wartTransfer(toAddr: string, wartE8: bigint): TransactionBytes {
+        const binary = Buffer.concat([
+            hashToBytes(this.chainPin.pinHash),
+            uint32BE(this.chainPin.pinHeight),
+            uint32BE(this.nonceId),
+            Buffer.alloc(3),
+            uint64BE(this.feeE8),
+            addressToBytes(toAddr),
+            uint64BE(wartE8),
+        ]);
+        return createTransactionBytes(binary);
+    }
+
+    tokenTransfer(
+        assetHash: string,
+        isLiquidity: boolean,
+        toAddr: string,
+        amountU64: bigint
+    ): TransactionBytes {
+        const binary = Buffer.concat([
+            hashToBytes(this.chainPin.pinHash),
+            uint32BE(this.chainPin.pinHeight),
+            uint32BE(this.nonceId),
+            Buffer.alloc(3),
+            uint64BE(this.feeE8),
+            hashToBytes(assetHash),
+            Buffer.from([isLiquidity ? 1 : 0]),
+            addressToBytes(toAddr),
+            uint64BE(amountU64),
+        ]);
+        return createTransactionBytes(binary);
+    }
+
+    limitSwap(
+        assetHash: string,
+        isBuy: boolean,
+        amountU64: bigint,
+        limit: string
+    ): TransactionBytes {
+        const binary = Buffer.concat([
+            hashToBytes(this.chainPin.pinHash),
+            uint32BE(this.chainPin.pinHeight),
+            uint32BE(this.nonceId),
+            Buffer.alloc(3),
+            uint64BE(this.feeE8),
+            hashToBytes(assetHash),
+            Buffer.from([isBuy ? 1 : 0]),
+            uint64BE(amountU64),
+            Buffer.from(limit, 'hex'),
+        ]);
+        return createTransactionBytes(binary);
+    }
+
+    liquidityDeposit(
+        assetHash: string,
+        amountU64: bigint,
+        wartE8: bigint
+    ): TransactionBytes {
+        const binary = Buffer.concat([
+            hashToBytes(this.chainPin.pinHash),
+            uint32BE(this.chainPin.pinHeight),
+            uint32BE(this.nonceId),
+            Buffer.alloc(3),
+            uint64BE(this.feeE8),
+            hashToBytes(assetHash),
+            uint64BE(amountU64),
+            uint64BE(wartE8),
+        ]);
+        return createTransactionBytes(binary);
+    }
+
+    liquidityWithdrawal(
+        assetHash: string,
+        amountE8: bigint
+    ): TransactionBytes {
+        const binary = Buffer.concat([
+            hashToBytes(this.chainPin.pinHash),
+            uint32BE(this.chainPin.pinHeight),
+            uint32BE(this.nonceId),
+            Buffer.alloc(3),
+            uint64BE(this.feeE8),
+            hashToBytes(assetHash),
+            uint64BE(amountE8),
+        ]);
+        return createTransactionBytes(binary);
+    }
+
+    cancelation(
+        cancelHeight: number,
+        cancelNonceId: number
+    ): TransactionBytes {
+        const binary = Buffer.concat([
+            hashToBytes(this.chainPin.pinHash),
+            uint32BE(this.chainPin.pinHeight),
+            uint32BE(this.nonceId),
+            Buffer.alloc(3),
+            uint64BE(this.feeE8),
+            uint32BE(cancelHeight),
+            uint32BE(cancelNonceId),
+        ]);
+        return createTransactionBytes(binary);
+    }
+
+    assetCreation(
+        supplyU64: bigint,
+        precision: number,
+        name: string
+    ): TransactionBytes {
+        const nameBuffer = Buffer.alloc(5);
+        nameBuffer.write(name, 'ascii');
+        const binary = Buffer.concat([
+            hashToBytes(this.chainPin.pinHash),
+            uint32BE(this.chainPin.pinHeight),
+            uint32BE(this.nonceId),
+            Buffer.alloc(3),
+            uint64BE(this.feeE8),
+            uint64BE(supplyU64),
+            Buffer.from([precision]),
+            nameBuffer,
+        ]);
+        return createTransactionBytes(binary);
+    }
+}
+
+export class TransactionBytes {
+    constructor(public readonly binary: Buffer) {}
+
+    hash(): string {
+        return createHash('sha256').update(this.binary).digest('hex');
+    }
+}
+
+const CREATE_KEY = Symbol('create');
+
+function createTransactionBytes(binary: Buffer): TransactionBytes {
+    return new TransactionBytes(binary);
 }
 
 function uint32BE(value: number): Buffer {
@@ -34,145 +171,4 @@ function addressToBytes(address: string): Buffer {
 
 function hashToBytes(hash: string): Buffer {
     return Buffer.from(hash, 'hex');
-}
-
-export class TransactionBytes {
-    constructor(public readonly binary: Buffer) {}
-
-    hash(): string {
-        return createHash('sha256').update(this.binary).digest('hex');
-    }
-
-    static wartTransfer(
-        context: TransactionContext,
-        toAddr: string,
-        wartE8: bigint
-    ): TransactionBytes {
-        const binary = Buffer.concat([
-            hashToBytes(context.chainPin.pinHash),
-            uint32BE(context.chainPin.pinHeight),
-            uint32BE(context.nonceId),
-            Buffer.alloc(3),
-            uint64BE(context.feeE8),
-            addressToBytes(toAddr),
-            uint64BE(wartE8),
-        ]);
-        return new TransactionBytes(binary);
-    }
-
-    static tokenTransfer(
-        context: TransactionContext,
-        assetHash: string,
-        isLiquidity: boolean,
-        toAddr: string,
-        amountU64: bigint
-    ): TransactionBytes {
-        const binary = Buffer.concat([
-            hashToBytes(context.chainPin.pinHash),
-            uint32BE(context.chainPin.pinHeight),
-            uint32BE(context.nonceId),
-            Buffer.alloc(3),
-            uint64BE(context.feeE8),
-            hashToBytes(assetHash),
-            Buffer.from([isLiquidity ? 1 : 0]),
-            addressToBytes(toAddr),
-            uint64BE(amountU64),
-        ]);
-        return new TransactionBytes(binary);
-    }
-
-    static limitSwap(
-        context: TransactionContext,
-        assetHash: string,
-        isBuy: boolean,
-        amountU64: bigint,
-        limit: string
-    ): TransactionBytes {
-        const binary = Buffer.concat([
-            hashToBytes(context.chainPin.pinHash),
-            uint32BE(context.chainPin.pinHeight),
-            uint32BE(context.nonceId),
-            Buffer.alloc(3),
-            uint64BE(context.feeE8),
-            hashToBytes(assetHash),
-            Buffer.from([isBuy ? 1 : 0]),
-            uint64BE(amountU64),
-            Buffer.from(limit, 'hex'),
-        ]);
-        return new TransactionBytes(binary);
-    }
-
-    static liquidityDeposit(
-        context: TransactionContext,
-        assetHash: string,
-        amountU64: bigint,
-        wartE8: bigint
-    ): TransactionBytes {
-        const binary = Buffer.concat([
-            hashToBytes(context.chainPin.pinHash),
-            uint32BE(context.chainPin.pinHeight),
-            uint32BE(context.nonceId),
-            Buffer.alloc(3),
-            uint64BE(context.feeE8),
-            hashToBytes(assetHash),
-            uint64BE(amountU64),
-            uint64BE(wartE8),
-        ]);
-        return new TransactionBytes(binary);
-    }
-
-    static liquidityWithdrawal(
-        context: TransactionContext,
-        assetHash: string,
-        amountE8: bigint
-    ): TransactionBytes {
-        const binary = Buffer.concat([
-            hashToBytes(context.chainPin.pinHash),
-            uint32BE(context.chainPin.pinHeight),
-            uint32BE(context.nonceId),
-            Buffer.alloc(3),
-            uint64BE(context.feeE8),
-            hashToBytes(assetHash),
-            uint64BE(amountE8),
-        ]);
-        return new TransactionBytes(binary);
-    }
-
-    static cancelation(
-        context: TransactionContext,
-        cancelHeight: number,
-        cancelNonceId: number
-    ): TransactionBytes {
-        const binary = Buffer.concat([
-            hashToBytes(context.chainPin.pinHash),
-            uint32BE(context.chainPin.pinHeight),
-            uint32BE(context.nonceId),
-            Buffer.alloc(3),
-            uint64BE(context.feeE8),
-            uint32BE(cancelHeight),
-            uint32BE(cancelNonceId),
-        ]);
-        return new TransactionBytes(binary);
-    }
-
-    static assetCreation(
-        context: TransactionContext,
-        supplyU64: bigint,
-        precision: number,
-        name: string
-    ): TransactionBytes {
-        const nameBuffer = Buffer.alloc(5);
-        nameBuffer.write(name, 'ascii');
-        const binary = Buffer.concat([
-            hashToBytes(context.chainPin.pinHash),
-            uint32BE(context.chainPin.pinHeight),
-            uint32BE(context.nonceId),
-            Buffer.alloc(3),
-            uint64BE(context.feeE8),
-            uint64BE(supplyU64),
-            Buffer.from([precision]),
-            nameBuffer,
-        ]);
-        return new TransactionBytes(binary);
-    }
 }
