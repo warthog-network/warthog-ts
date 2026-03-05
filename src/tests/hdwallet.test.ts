6a9fbd79be815944590fafe9a7a92f2908e6983f
CoinFu Master Shifu <145641460+CoinFuMasterShifu@users.noreply.github.com>
2026-03-05 09:29:53 +0100

    remove useless tests

--- src/tests/hdwallet.test.ts
+++ src/tests/hdwallet.test.ts
@@ -2,18 +2,12 @@ import { test, expect } from "bun:test";
 import { HDWallet } from "../types/HDWallet";
 import { Account } from "../types/Account";
 
-test("HDWallet.fromMnemonic creates valid instance", () => {
+test("HDWallet.fromMnemonic return valid Account", () => {
   const mnemonic =
     "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
   const wallet = HDWallet.fromMnemonic(mnemonic);
 
   expect(wallet).not.toBeNull();
-});
-
-test("HDWallet.deriveAccountAtIndex returns valid Account", () => {
-  const mnemonic =
-    "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
-  const wallet = HDWallet.fromMnemonic(mnemonic);
   const account = wallet.deriveAccountAtIndex(0);
 
   expect(account.getAddress().length).toBe(48);
@@ -40,13 +34,4 @@ test("HDWallet full path derivation matches index method", () => {
   const accountFromPath = wallet.deriveAccountFromPath("0/0");
 
   expect(accountFromIndex.getAddress()).toBe(accountFromPath.getAddress());
-});
-
-test("HDWallet deriveAccountAtIndex validates account checksum", () => {
-  const mnemonic =
-    "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
-  const wallet = HDWallet.fromMnemonic(mnemonic);
-  const account = wallet.deriveAccountAtIndex(0);
-
-  expect(Account.validate_address(account.getAddress())).toBe(true);
 });
